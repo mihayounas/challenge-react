7caa1d1de232eb193082d9c21c0e523a63a4eae2
Mihaela Younas <mihaelayounas55@gmail.com>
2022-11-02 11:01:30 +0000

    work on chanllenge and create functional component and import it

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Sidebar() {
+    return (
+        <div>
+        <a href="#">My Photos</a>
+        <a href="#">My Illustrations</a>
+        <a href="#">My Paintings</a>
+        </div>
+    );
+  }
+
+
+
+export default Sidebar;
